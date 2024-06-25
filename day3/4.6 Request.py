938a473664c8ffd18e9ae8dba7d28578029752cc
sakai04 <134619114+Sakai04@users.noreply.github.com>
2024-06-25 21:35:17 +0900

    Update 4.6 Request.py

--- day3/4.6 Request.py
+++ day3/4.6 Request.py
@@ -8,6 +8,6 @@ website = ( "http://apple.com",
 
 for w in website: # Loop through the tuple
     if not w.startswith("http://"):# if the item does not start with "http://"
-        w = f"http://{w}"# add "http://" to the beginning of the item
+        w = f"http://{w}"# f스트링으로 더해줌
 
 
