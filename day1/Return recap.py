833f4035a00cd76f5040b94c4cffa39d721f4818
sakai04 <134619114+Sakai04@users.noreply.github.com>
2024-06-28 22:24:03 +0900

    Return recap.py 업데이트

--- day1/Return recap.py
+++ day1/Return recap.py
@@ -2,6 +2,6 @@ my_name="Walter White"
 my_age=50
 my_hair_style="bald"
 
-print(f"hello I'm {my_name}. "
+print(f"hello I'm {my_name}. " # f스트링
       f"I am {my_age} years old and "
       f"my hair is {my_hair_style}")
