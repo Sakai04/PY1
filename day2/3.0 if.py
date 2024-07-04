037881c14bc2b936d2ec16b30bb15d0d18df328a
sakai04 <134619114+Sakai04@users.noreply.github.com>
2024-07-05 00:46:42 +0900

    Update 3.0 if.py

--- day2/3.0 if.py
+++ day2/3.0 if.py
@@ -2,5 +2,5 @@ a = "Walter White" # a 선언
 
 if a == "Walter White": # a가 Walter White와 같다면
     print("Breaking Bad") #  Breaking Bad 출력
-else: # 그게 아니라면
+else: # 그게 아니라면..
     print("this is not mathamphetamine") # this is not mathamphetamine 출력
