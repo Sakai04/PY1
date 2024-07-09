52820d174d9fc445272f92018d9773c897155c61
sakai04 <134619114+Sakai04@users.noreply.github.com>
2024-07-09 21:17:01 +0900

    Update 4.2 Tuple.py

--- day3/4.2 Tuple.py
+++ day3/4.2 Tuple.py
@@ -1,2 +1,2 @@
-drugs = ("pnt"cocaine", "heroin", "marijuana", "methamphetamine", "ecstasy") # tuple
-print(drugs[3]) # tuple의 세 번째 item을 출력한다. # methamphetamine
+drugs = ("pnt"cocaine", "heroin", "marijuana", "methamphetamine", "ecstasy") # tuple 튜플
+print(drugs[3]) # tuple의 세 번째 item을 출력한다. # methamphetamine 메스암페타민
