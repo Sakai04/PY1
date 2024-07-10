2e453e446c629d25c48c8251bf988162a60e7e49
sakai04 <134619114+Sakai04@users.noreply.github.com>
2024-07-10 21:18:18 +0900

    3.3 Python Standard Library.py 업데이트

--- day2/3.3 Python Standard Library.py
+++ day2/3.3 Python Standard Library.py
@@ -3,7 +3,7 @@ from random import randint #라이브러리 호출
 User_choice = int(input("Enter your choice: "))
 pc_choice = randint(1,100)
 
-if User_choice == pc_choice:
+if User_choice == pc_choice:#if 문
     print("It's a draw")
 elif User_choice > pc_choice:
     print("You win, com choose:", pc_choice)
